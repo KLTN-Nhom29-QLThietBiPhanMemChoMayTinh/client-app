eabb366a27496017756bf91a8de1769e604a78cb
AVan-Git <hoangkc35@gmail.com>
2023-10-25 09:56:10 +0700

    add lib yup(regex): sd useFormik()

--- src/pages/PageLogin/PageLogin.jsx
+++ src/pages/PageLogin/PageLogin.jsx
@@ -1,7 +1,38 @@
+import { useFormik } from "formik";
 import React from "react";
+import * as Yup from 'yup'
 import { FaRegCopyright } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 
 export default function PageLogin() {
+
+
+  const dispatch = useDispatch();
+  const REGEX_PASSWORD= /^(?=.*\d)(?=.*[a-zA-Z])[\da-zA-Z_.\-@]{6,}$/;
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: Yup.object().shape({
+      username: Yup.string().trim().ensure()
+              .required("Tài khoản không được bỏ trống!")
+              .min(3,"Tài khoản trên 3 ký tự!")
+              .matches(/^[a-z0-9_-]{3,16}$/, "Tài khoản không có ký tự đặc biệt!"),
+              
+      password: Yup.string().required("Mật khẩu không được bỏ trống!")
+            .min(6, 'Mật khẩu từ 6 - 32 ký tự !')
+            .max(32,'Mật khẩu từ 6 - 32 ký tự !')
+            .matches(REGEX_PASSWORD, 'Mật khẩu có ít nhất một chữ số và một chữ cái!')
+            
+    }),
+    onSubmit:(value) => {
+      console.log("🚀 ~ file: PageLogin.jsx:17 ~ PageLogin ~ value:", value)
+      
+    }
+  })
+
   return (
     <div
       className="d-flex flex-column justify-content-around align-items-center"
@@ -9,8 +40,9 @@ export default function PageLogin() {
     >
       <h1>Hệ thông quản lý phòng máy công nghệ thông tin</h1>
       <form
-        action="#"
-        method="post"
+        // action="#"
+        // method="post"
+        onSubmit={formik.handleSubmit}
         className="col-md-5 d-flex flex-column align-items-center p-3 opacity_05"
         style={{ height: "400px" }}
       >
@@ -19,8 +51,8 @@ export default function PageLogin() {
         <div className="mb-3 w-75 form-group">
           <label htmlFor="username" className="form-label ms-2 mb-0" >
             Tài khoản
-            <small id="errUsername" className="form-text text-danger ms-2">
-              {/* {formik.errors.email? formik.errors.email:'' } */}
+            <small id="errUsername" className="form-text text_color_yellow ms-2">
+              {formik.errors.username? formik.errors.username:'' }
             </small>
           </label>
           <input
@@ -30,15 +62,15 @@ export default function PageLogin() {
             id="username"
             aria-describedby="errUsername"
             placeholder="Nhập tài khoản..."
-            //   onBlur={formik.handleBlur}
-            //   onChange={formik.handleChange}
+              // onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
           />
         </div>
         <div className="mb-3 w-75 form-group">
           <label htmlFor="password" className="form-label ms-2 mb-0">
             Mật khẩu
-            <small id="errPassword" className="form-text text-danger ms-2">
-              {/* {formik.errors.password ? formik.errors.password : ""} */}
+            <small id="errPassword" className="form-text text_color_yellow ms-2" >
+              {formik.errors.password ? formik.errors.password : ""}
             </small>
           </label>
           <input
@@ -48,7 +80,7 @@ export default function PageLogin() {
             id="password"
             aria-describedby="errPassword"
             placeholder="Nhập mật khẩu..."
-            // onChange={formik.handleChange}
+            onChange={formik.handleChange}
             // onBlur={formik.handleBlur}
           />
         </div>
