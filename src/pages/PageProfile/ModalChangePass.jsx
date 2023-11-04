1bd10b319906e72e3a10ed4d68fea208553343b5
AVan-Git <hoangkc35@gmail.com>
2023-11-04 20:06:38 +0700

    PageProfile add regex pass

--- src/pages/PageProfile/ModalChangePass.jsx
+++ src/pages/PageProfile/ModalChangePass.jsx
@@ -1,6 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-zA-Z])[\da-zA-Z_.\-@]{6,}$/;
 
 export default function ModalChangePass(props) {
+
+  let [password, setPassword] = useState({
+    pass_old: '',
+    pass_new: '',
+    pass_new2: ''
+  })
+  let [errPass, setErrorPass] = useState({
+    pass_old: '',
+    pass_new: '',
+    pass_new2: ''
+  })
+
+  // handle
+  const handleChangePass = (e) => {
+    const regex = new RegExp(REGEX_PASSWORD);
+    let {id,value} = e.target;
+
+    setPassword({...password, [id] : value})
+
+
+    if ([id] == 'pass_new2') {
+      if (value !== password.pass_new && value.length >=6 ) {
+        setErrorPass({...errPass, pass_new2:'Hai mật khẩu mới phải giống nhau!'})
+      }
+      else if(value === password.pass_new)
+      {
+        setErrorPass({...errPass, pass_new2:''})
+      }
+    }
+    else{
+      let booleanRegex = regex.test(value);
+      if(!booleanRegex && value.length >=6 )
+      {
+          setErrorPass({...errPass, [id]:'Mật khẩu trên 6 ký tự có ít nhất một chữ cái hoắc chữ số!'})
+      }
+      else if(booleanRegex)
+      {
+        setErrorPass({...errPass, [id]:''})
+      }
+    }
+    if ([id] == 'pass_new') {
+      if (value === password.pass_new2 && value.length >=6 ) 
+      {
+        setErrorPass({...errPass, pass_new2:''})
+      }
+      else if(value !== password.pass_new2 && password.pass_new2.length >=6){
+        setErrorPass({...errPass, pass_new2:'Hai mật khẩu mới phải giống nhau!'})
+      }
+    }
+  }
+  //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(password);
+    if(checkData()) {
+      // true
+    }
+    // false
+  }
+  // 
+  const checkData = () => {
+    const regex = new RegExp(REGEX_PASSWORD);
+    // let booleanRegex = regex.test(value);
+
+    /**
+     * duyet obj 
+     - so sanh vs regex
+     - xxuat loi
+     - check data cu vs data moi 
+     - gui data redux
+     - cal Api
+     */
+    return true;
+  }
+
   return (
     <>
       {/* Modal */}
@@ -23,45 +100,70 @@ export default function ModalChangePass(props) {
                 className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
+                onClick={() => {
+                  setErrorPass({
+                    pass_old: '',
+                    pass_new: '',
+                    pass_new2: ''
+                  });
+                  setPassword({
+                    pass_old: '',
+                    pass_new: '',
+                    pass_new2: ''
+                  });
+                }}
               />
             </div>
             <div className="modal-body">
               <div className="mb-3">
                 <label htmlFor="pass_old" className="form-label">
                   Mật khẩu cũ
-                  <small id="helpPass_old" className="mx-2 form-text text-danger">*</small>
+                  <small id="helpPass_old" className="mx-2 form-text text-danger">*{errPass.pass_old}</small>
                 </label>
-                <input type="password" className="form-control" name="pass_old" id="pass_old" aria-describedby="helpPass_old" placeholder="Mật khẩu cũ" />
+                <input onChange={handleChangePass} value={password.pass_old} type="password" className="form-control" name="pass_old" id="pass_old" aria-describedby="helpPass_old" placeholder="Mật khẩu cũ" />
               </div>
               
               <div className="mb-3">
                 <label htmlFor="pass_new" className="form-label">
                   Mật khẩu mới
-                  <small id="helpPass_new" className="mx-2 form-text text-danger">*</small>
+                  <small id="helpPass_new" className="mx-2 form-text text-danger">*{errPass.pass_new}</small>
                 </label>
-                <input type="password" className="form-control" name="pass_new" id="pass_new" aria-describedby="helpPass_new" placeholder="Mật khẩu mới" />
+                <input  onChange={handleChangePass} value={password.pass_new}  type="password" className="form-control" name="pass_new" id="pass_new" aria-describedby="helpPass_new" placeholder="Mật khẩu mới" />
               </div>
               
               <div className="mb-3">
                 <label htmlFor="pass_new2" className="form-label">
                   Nhập lại mật khẩu
-                  <small id="helpPass_new2" className="mx-2 form-text text-danger">*</small>
+                  <small id="helpPass_new2" className="mx-2 form-text text-danger">*{errPass.pass_new2}</small>
                 </label>
-                <input type="password" className="form-control" name="pass_new2" id="pass_new2" aria-describedby="helpPass_new2" placeholder="Mật khẩu mới" />
+                <input  onChange={handleChangePass} value={password.pass_new2}  type="password" className="form-control" name="pass_new2" id="pass_new2" aria-describedby="helpPass_new2" placeholder="Mật khẩu mới" />
               </div>
 
 
             </div>
             <div className="modal-footer">
               <button
-                type="button"
+                type="reset"
                 className="btn btn-secondary"
-                data-bs-dismiss="modal"
+                onClick={() => {
+                  setErrorPass({
+                    pass_old: '',
+                    pass_new: '',
+                    pass_new2: ''
+                  });
+                  setPassword({
+                    pass_old: '',
+                    pass_new: '',
+                    pass_new2: ''
+                  });
+                }}
               >
-                Thoát
+                Làm mới
               </button>
-              <button type="button" className="btn btn-primary">
-                Lưu
+              <button 
+                onClick={handleSubmit}
+                type="submit" className="btn btn-primary">
+                Chỉnh sửa
               </button>
             </div>
           </div>
--- src/pages/PageProfile/PageProfile.jsx
+++ src/pages/PageProfile/PageProfile.jsx
@@ -16,130 +16,143 @@ export default function PageProfile() {
     let { quyen } = userLogin.taiKhoan;
 
     if (quyen.tenQuyen.toLowerCase().includes("Giáo viên".toLowerCase())) {
-      return <>
-        <table className="w-100">
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Họ và tên:
-          </td>
-          <td className="col-md-9 px-2 text-capitalize">{userLogin.hoTen}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Email:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.email}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Phone:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.soDienThoai}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Tài khoản:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.taiKhoan.tenDangNhap}</td>
-        </tr>
-        <tr className="p-2 row d-flex align-items-center">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Mật khẩu:
-          </td>
-          <td className="col-md-9 px-2 row">
-            <span className="col-8 pt-2 ">****************</span>
-            <span
-              className="col-1 btn_moune"
-              data-bs-toggle="modal"
-              data-bs-target="#modalId"
-            >
-              <img src="./img/icon_changepass.png" width={25} alt="..." />
-            </span>
-          </td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Quyền:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.taiKhoan.quyen.tenQuyen}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Khoa:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.khoa.tenKhoa}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Học vị:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.hocVi}</td>
-        </tr>
-        
-      </table>
-      </>;
+      return (
+        <>
+          <table className="w-100">
+            <tbody>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  <span>Họ và tên:</span>
+                </td>
+                <td className="col-md-9 px-2 text-capitalize">
+                  <span>{userLogin.hoTen}</span>
+                </td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Email:
+                </td>
+                <td className="col-md-9 px-2">{userLogin.email}</td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Phone:
+                </td>
+                <td className="col-md-9 px-2">{userLogin.soDienThoai}</td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Tài khoản:
+                </td>
+                <td className="col-md-9 px-2">
+                  {userLogin.taiKhoan.tenDangNhap}
+                </td>
+              </tr>
+              <tr className="p-2 row d-flex align-items-center">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Mật khẩu:
+                </td>
+                <td className="col-md-9 px-2 row">
+                  <span className="col-8 pt-2 ">****************</span>
+                  <span
+                    className="col-1 btn_moune"
+                    data-bs-toggle="modal"
+                    data-bs-target="#modalId"
+                  >
+                    <img src="./img/icon_changepass.png" width={25} alt="..." />
+                  </span>
+                </td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Quyền:
+                </td>
+                <td className="col-md-9 px-2">
+                  {userLogin.taiKhoan.quyen.tenQuyen}
+                </td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Khoa:
+                </td>
+                <td className="col-md-9 px-2">{userLogin.khoa.tenKhoa}</td>
+              </tr>
+              <tr className="p-2 row">
+                <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+                  Học vị:
+                </td>
+                <td className="col-md-9 px-2">{userLogin.hocVi}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      );
     }
 
     return (
       <table className="w-100">
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Họ và tên:
-          </td>
-          <td className="col-md-9 px-2 text-capitalize">{userLogin.tenNV}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Email:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.email}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Phone:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.sDT}</td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Tài khoản:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.taiKhoan.tenDangNhap}</td>
-        </tr>
-        <tr className="p-2 row d-flex align-items-center">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Mật khẩu:
-          </td>
-          <td className="col-md-9 px-2 row">
-            <span className="col-8 pt-2 ">****************</span>
-            <span
-              className="col-1 btn_moune"
-              data-bs-toggle="modal"
-              data-bs-target="#modalId"
-            >
-              <img src="./img/icon_changepass.png" width={25} alt="..." />
-            </span>
-          </td>
-        </tr>
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Quyền:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.taiKhoan.quyen.tenQuyen}</td>
-        </tr>
+        <tbody>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Họ và tên:
+            </td>
+            <td className="col-md-9 px-2 text-capitalize">{userLogin.tenNV}</td>
+          </tr>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Email:
+            </td>
+            <td className="col-md-9 px-2">{userLogin.email}</td>
+          </tr>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Phone:
+            </td>
+            <td className="col-md-9 px-2">{userLogin.sDT}</td>
+          </tr>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Tài khoản:
+            </td>
+            <td className="col-md-9 px-2">{userLogin.taiKhoan.tenDangNhap}</td>
+          </tr>
+          <tr className="p-2 row d-flex align-items-center">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Mật khẩu:
+            </td>
+            <td className="col-md-9 px-2 row">
+              <span className="col-8 pt-2 ">****************</span>
+              <span
+                className="col-1 btn_moune"
+                data-bs-toggle="modal"
+                data-bs-target="#modalId"
+              >
+                <img src="./img/icon_changepass.png" width={25} alt="..." />
+              </span>
+            </td>
+          </tr>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Quyền:
+            </td>
+            <td className="col-md-9 px-2">
+              {userLogin.taiKhoan.quyen.tenQuyen}
+            </td>
+          </tr>
 
-        <tr className="p-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Chức vụ:
-          </td>
-          <td className="col-md-9 px-2">{userLogin.chucVu.tenCV}</td>
-        </tr>
-        <tr className="px-2 row">
-          <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
-            Mô tả công việc:
-          </td>
-          <td className="col-md-9 px-2">Không có</td>
-        </tr>
+          <tr className="p-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Chức vụ:
+            </td>
+            <td className="col-md-9 px-2">{userLogin.chucVu.tenCV}</td>
+          </tr>
+          <tr className="px-2 row">
+            <td className="col-md-3 fs-6" style={{ fontWeight: "550" }}>
+              Mô tả công việc:
+            </td>
+            <td className="col-md-9 px-2">Không có</td>
+          </tr>
+        </tbody>
       </table>
     );
   };
@@ -158,7 +171,9 @@ export default function PageProfile() {
           <div style={{ height: "80vh" }}>
             {/*  */}
             <div style={{ height: "8vh" }}>
-              <NavTab itemLink={{ arrLinkNavTab, chucNang: "Thông tin tài khoản" }} />
+              <NavTab
+                itemLink={{ arrLinkNavTab, chucNang: "Thông tin tài khoản" }}
+              />
             </div>
 
             {/*  */}
