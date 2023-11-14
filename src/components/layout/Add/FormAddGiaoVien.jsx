23783ca6d14ea03707732146c8a1725a3fcef32b
AVan-Git <hoangkc35@gmail.com>
2023-11-14 19:27:55 +0700

    FormAddGV: chua xong CheckData()

--- src/components/layout/Add/FormAddGiaoVien.jsx
+++ src/components/layout/Add/FormAddGiaoVien.jsx
@@ -1,13 +1,150 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "../../common/Footer/Footer";
 import NavTab from "../../common/NavTab/NavTab";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllKhoaApi } from "../../../redux/reducers/khoaReducer";
 
 export default function FormAddGiaoVien() {
+  const dispatch = useDispatch();
+  //
+  let { arrKhoa } = useSelector((state) => state.khoaReducer);
 
-  let {arrKhoa } = useSelector(state => state.khoaReducer);
+  //
+  let objGiaoVien = useRef({
+    hoTen: "",
+    soDienThoai: "",
+    email: "",
+    hocVi: "Thạc sĩ",
+    taiKhoan: {},
+    khoa: {},
+  });
+  let [errGiaoVien, setErrGiaoVien] = useState({
+    hoTen: "",
+    soDienThoai: "",
+    email: "",
+    khoa: "",
+  });
 
-  
+  //
+  useEffect(() => {
+    if (arrKhoa.length === 0) {
+      dispatch(getAllKhoaApi);
+    } else {
+      objGiaoVien.current = {
+        hoTen: "",
+        soDienThoai: "",
+        email: "",
+        hocVi: "Thạc sĩ",
+        taiKhoan: {},
+        khoa: arrKhoa[0],
+      };
+    }
+  }, []);
+
+  // handle
+  const handleChangeText = (e) => {
+    let { id, value } = e.target;
+
+    objGiaoVien.current[id] = value;
+
+    if (value.trim().length === 0) {
+      setErrGiaoVien({ ...errGiaoVien, [id]: "Hãy nhập thông tin!" });
+    } else {
+      setErrGiaoVien({ ...errGiaoVien, [id]: "" });
+    }
+  };
+  //
+  const handleChangeHocVi = (e) => {
+    objGiaoVien.current.hocVi = e.target.value;
+  };
+  //
+  const handleChangeKhoa = (e) => {
+    let idKhoa = e.target.value;
+
+    let arrdata = arrKhoa.filter((item) => item.maKhoa == idKhoa);
+
+    objGiaoVien.current.khoa = arrdata[0];
+  };
+  //
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(objGiaoVien.current);
+    if (checkData()) {
+      // true
+    }
+    // false
+  };
+
+  //
+  const checkData = () => {
+    let result = 1;
+
+    let errhoTen = "";
+    let errsoDienThoai = "";
+    let erremail = "";
+    let errkhoa = "";
+
+    let regexEmail = new RegExp(
+      "^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$"
+    );
+    let regexPhone = new RegExp(
+      "\d{9,12}"
+    );
+
+    let { hoTen, soDienThoai, email, khoa } = objGiaoVien.current;
+
+    console.log(regexPhone.test(soDienThoai));
+
+    if (hoTen.trim().length === 0) {
+      errhoTen = "Hãy nhập thông tin!";
+    }
+    if (soDienThoai.trim().length === 0) {
+      errsoDienThoai = "Hãy nhập thông tin!";
+    } else if (soDienThoai.trim().length < 9 || soDienThoai.trim().length > 15) {
+      errsoDienThoai = "Số điện thoại có trên 9 số và nhỏ hơn 15 số!";
+    }
+    
+    
+    // if(regexPhone.test(soDienThoai)) {
+    // }
+
+    setErrGiaoVien({
+      hoTen: errhoTen,
+      soDienThoai: errsoDienThoai,
+      email: erremail,
+      khoa: errkhoa,
+    })
+
+    return result;
+  };
+
+  // render
+  const renderSelectKhoa = () => {
+    if (Object.keys(objGiaoVien.current.khoa).length === 0) {
+      return (
+        <>
+          <option value={-1} selected>
+            Tất cả
+          </option>
+          {arrKhoa.map((item, index) => {
+            return (
+              <option key={index} value={item.maKhoa}>
+                {item.tenKhoa}
+              </option>
+            );
+          })}
+        </>
+      );
+    }
+
+    return arrKhoa.map((item, index) => {
+      return (
+        <option key={index} value={item.maKhoa}>
+          {item.tenKhoa}
+        </option>
+      );
+    });
+  };
 
   // Mảng quản lý data navtab
   let arrLinkNavTab = [
@@ -31,79 +168,88 @@ export default function FormAddGiaoVien() {
               style={{ height: "82vh" }}
             >
               {/* Form add */}
-              <form className="d-flex h-100 justify-content-between flex-column">
+              <form
+                onSubmit={handleSubmit}
+                className="d-flex h-100 justify-content-between flex-column"
+              >
                 <div className="">
-                  <h2 className="mx-3 mb-3 text-center">Thêm giáo viên</h2>
+                  <h2 className="mx-3 mb-3 ">Thêm giáo viên</h2>
                   <div className="row">
                     {/* left */}
                     <div className="col-md-6">
                       {/* input ten GV */}
                       <div className="mb-3 col-md-9">
-                        <label htmlFor="txtTenGV" className="form-label ms-2">
+                        <label htmlFor="hoTen" className="form-label ms-2">
                           Họ và tên
                           <small
                             id="errTenGV"
                             className="form-text text-danger mx-2"
                           >
-                            *
+                            *{errGiaoVien.hoTen}
                           </small>
                         </label>
                         <input
                           type="text"
                           className="form-control p-2"
-                          name="txtTenGV"
-                          id="txtTenGV"
+                          name="hoTen"
+                          id="hoTen"
                           aria-describedby="errTenGV"
                           placeholder="Nguyễn Văn A..."
+                          onChange={handleChangeText}
                         />
                       </div>
 
                       {/* input email GV */}
                       <div className="mb-3 col-md-9">
-                        <label htmlFor="txtEmail" className="form-label ms-2">
+                        <label htmlFor="email" className="form-label ms-2">
                           Địa chỉ email
                           <small
                             id="errEmail"
                             className="form-text text-danger mx-2"
                           >
-                            *
+                            *{errGiaoVien.email}
                           </small>
                         </label>
                         <input
                           type="email"
                           className="form-control p-2"
-                          name="txtEmail"
-                          id="txtEmail"
+                          name="email"
+                          id="email"
                           aria-describedby="errEmail"
                           placeholder="AVan@gmail.com"
+                          onChange={handleChangeText}
                         />
                       </div>
 
                       {/* input phone GV */}
                       <div className="mb-3 col-md-9">
-                        <label htmlFor="txtSoDT" className="form-label ms-2">
+                        <label
+                          htmlFor="soDienThoai"
+                          className="form-label ms-2"
+                        >
                           Số điện thoại
                           <small
                             id="errSoDT"
                             className="form-text text-danger mx-2"
                           >
-                            *
+                            *{errGiaoVien.soDienThoai}
                           </small>
                         </label>
                         <input
                           type="tel"
                           className="form-control p-2"
-                          name="txtSoDT"
-                          id="txtSoDT"
+                          name="soDienThoai"
+                          id="soDienThoai"
                           aria-describedby="errSoDT"
                           placeholder="0951753001"
+                          onChange={handleChangeText}
                         />
                       </div>
                     </div>
                     <div className="col-md-6">
                       {/* select Học vị */}
                       <div className="mb-3 col-md-9">
-                        <label htmlFor="selHocVi ms-2" className="form-label">
+                        <label htmlFor="selHocVi" className="form-label ms-2">
                           Học vị
                           <small className="form-text text-danger mx-2">
                             *
@@ -113,10 +259,11 @@ export default function FormAddGiaoVien() {
                           className="form-select p-2 form-select-ms"
                           name="selHocVi"
                           id="selHocVi"
+                          onChange={handleChangeHocVi}
                         >
-                          <option value>Thạc sĩ</option>
-                          <option value>Tiến sĩ</option>
-                          <option value>Giáo sư</option>
+                          <option value="Thạc sĩ">Thạc sĩ</option>
+                          <option value="Tiến sĩ">Tiến sĩ</option>
+                          <option value="Giáo sư">Giáo sư</option>
                         </select>
                       </div>
 
@@ -125,22 +272,18 @@ export default function FormAddGiaoVien() {
                         <label htmlFor="selKhoa" className="form-label ms-2">
                           Khoa công tác
                           <small className="form-text text-danger mx-2">
-                            *
+                            *{errGiaoVien.khoa}
                           </small>
                         </label>
                         <select
                           className="form-select p-2 form-select-ms"
                           name="selKhoa"
                           id="selKhoa"
+                          onChange={handleChangeKhoa}
                         >
-                          <option value={-1} selected>Tất cả</option>
-                          <option value>New Delhi</option>
-                          <option value>Istanbul</option>
-                          <option value>Jakarta</option>
+                          {renderSelectKhoa()}
                         </select>
                       </div>
-
-
                     </div>
                   </div>
                 </div>
