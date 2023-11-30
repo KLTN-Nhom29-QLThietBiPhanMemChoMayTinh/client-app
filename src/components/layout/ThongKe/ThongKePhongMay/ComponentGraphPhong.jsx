09845dfa9ffe29c9aace8ae17c5dcf50bfea9246
AVan-Git <hoangkc35@gmail.com>
2023-12-01 01:48:00 +0700

    TK Phong máy: finish 2 graph

--- src/components/layout/ThongKe/ThongKePhongMay/ComponentGraphPhong.jsx
+++ src/components/layout/ThongKe/ThongKePhongMay/ComponentGraphPhong.jsx
@@ -1,16 +1,159 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+import { useSelector } from "react-redux";
+
+let data = [
+  { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
+  { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
+  { name: "Page C", uv: 2000, pv: 11800, amt: 2290 },
+  { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
+  { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
+  { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+  { name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
+];
 
 export default function ComponentGraphPhong() {
+  //
+  const { arrTK_DataPhong, sum_PM, arrTK_DataPhong_Search } = useSelector(
+    (state) => state.thongkePhongReducer
+  );
+  //
+
+  // render
+  const renderTK_Phong_MayTinh = () => {
+    if (arrTK_DataPhong.length === 0) {
+      return <></>;
+    }
+    let rowItem = 0;
+    let arrGraph = [];
+    arrTK_DataPhong_Search.forEach((item) => {
+      rowItem++;
+      if (rowItem >= 9) return;
+      let { tenPhong, soMayTinh, soMT_err } = item;
+      arrGraph.push({ tenPhong, "Số máy": soMayTinh, "Số máy hỏng": soMT_err });
+    });
+
+    return (
+      <ResponsiveContainer className="col-md-8 mt-5" height={400}>
+        <BarChart
+          width={500}
+          height={300}
+          data={arrGraph}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="tenPhong" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="Số máy"
+            fill="#8884d8"
+            activeBar={<Rectangle fill="pink" stroke="blue" />}
+          />
+          <Bar
+            dataKey="Số máy hỏng"
+            fill="#82ca9d"
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+          />
+        </BarChart>
+        <p className="text-center w-100">
+          Biểu đồ 1: Thống kê phòng theo số máy quản lý
+        </p>
+      </ResponsiveContainer>
+    );
+  };
+  //
+  const renderTK_Phong_PhanMem = () => {
+    if (arrTK_DataPhong.length === 0) {
+      return <></>;
+    }
+    let rowItem = 0;
+    let arrGraph = [];
+    arrTK_DataPhong_Search.forEach((item) => {
+      rowItem++;
+      if (rowItem >= 9) return;
+      let { tenPhong, soPhanMem, soPM_err } = item;
+      arrGraph.push({ tenPhong, "Số phần mềm": soPhanMem, "Số PM bị lỗi": soPM_err });
+    });
+
+    return (
+      <ResponsiveContainer className="col-md-8 mt-5" height={400}>
+        <BarChart
+          width={500}
+          height={300}
+          data={arrGraph}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="tenPhong" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="Số phần mềm"
+            fill="#8884d8"
+            activeBar={<Rectangle fill="pink" stroke="blue" />}
+          />
+          <Bar
+            dataKey="Số PM bị lỗi"
+            fill="#82ca9d"
+            activeBar={<Rectangle fill="gold" stroke="purple" />}
+          />
+        </BarChart>
+        <p className="text-center w-100">
+          Biểu đồ 2: Thống kê phòng theo số phần mềm quản lý
+        </p>
+      </ResponsiveContainer>
+    );
+  };
+  //
   return (
     <div className="mb-5">
-      <div className="d-flex justify-content-around flex-wrap">
-        {/* {renderGraph_ToaNha()} */}
-      </div>
-      <div className="d-flex justify-content-around flex-wrap">
-        {/* <GraphTKToaNhaTheoTrangThai_SimpleBarChart
-          data={tk_ToaNha_status_mayTinh}
-        /> */}
+      {/*  */}
+      <>{renderTK_Phong_MayTinh()}</>
+      {/*  */}
+      <>{renderTK_Phong_PhanMem()}</>
+      
+      
+      <div className="col-5 mt-5">
+        <strong>Ghi Chú</strong>
+        <ul>
+          <li>PM: phần mềm</li>
+          <li>Biểu đồ đo theo thời gian hiện tại</li>
+          <li>
+            Biểu đồ được bị ảnh hưởng số liệu theo bảng 1 bên số liệu ( thống kê theo phòng )
+          </li>
+        </ul>
       </div>
     </div>
   );
 }
+
+//
+const sort_soMayTinh = (a, b) => {
+  let valuea = a.soMayTinh;
+  let valueb = b.soMayTinh;
+  return valueb - valuea;
+};
--- src/components/layout/ThongKe/ThongKePhongMay/ComponentTablePhong.jsx
+++ src/components/layout/ThongKe/ThongKePhongMay/ComponentTablePhong.jsx
@@ -88,16 +88,16 @@ export default function ComponentTablePhong() {
   };
   //
   const renderNhanXet = () => {
-    let arrNhanXet = [...arrNhanXet_MT, ...arrNhanXet_PM]
+    let arrNhanXet = [...arrNhanXet_MT, ...arrNhanXet_PM];
     return arrNhanXet?.map((item, index) => {
       let { title, name, tile, soLuong_err } = item;
-      let str = `${name} có ${soLuong_err} ${title} bị hỏng, chiếm ${tile}% tổng số ${title}`
+      let str = `${name} có ${soLuong_err} ${title} bị hỏng, chiếm ${tile}% tổng số ${title}`;
       return <li key={index}>{str}</li>;
     });
   };
   //
   return (
-    <div>
+    <>
       <div className="table-responsive-md mt-5 ">
         <strong>Bảng 1: Thống kê theo phòng máy( thời gian hiện tại ) </strong>
         <table className="table table-striped table-hover table-bordered">
@@ -147,9 +147,7 @@ export default function ComponentTablePhong() {
                 Tổng
               </td>
               <td className="text-center fw-bold">{sum_PM} </td>
-              <td className="text-center fw-bold">
-                {sumPM_err} ( {((sumPM_err * 100) / sum_PM).toFixed(2)}% ){" "}
-              </td>
+              <td className="text-center fw-bold">{sumPM_err}</td>
               <td className="text-center fw-bold">{sumMayTinh} (máy)</td>
               <td className="text-center fw-bold">
                 {sumMayTinh_err} ({" "}
@@ -165,10 +163,7 @@ export default function ComponentTablePhong() {
             <ul>
               <li>PM: phần mềm</li>
               <li>Tổng số ứng dụng PM: tổng số phần mềm trong hệ thống có.</li>
-              <li>
-                Tổng số PM bị lỗi A (B%): A - tổng số PM bị lỗi có trong danh
-                sách; B - tị lệ PM lỗi với tổng số PM.
-              </li>
+              <li>Tổng số PM bị lỗi: tổng số PM bị lỗi có trong danh sách.</li>
               <li>Tổng số máy: tổng số máy có trong danh sách trên.</li>
               <li>
                 Tổng số máy bị hỏng A (B%): A - tổng số máy bị hỏng có trong
@@ -182,52 +177,6 @@ export default function ComponentTablePhong() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-//
-const TableTKTang = (props) => {
-  let { index, sum, data_table, text_name, name_title } = props.data;
-
-  return (
-    <div key={index} className="table-responsive-md mt-5 ">
-      <strong>Bảng {name_title} </strong>
-      <table className="table table-striped table-hover table-bordered">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Tên tầng</th>
-            <th>Số {text_name}</th>
-            <th>Tỉ lệ</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data_table?.map((item, index1) => {
-            let ti_le = (item.value / sum) * 100;
-            return (
-              <tr key={index1}>
-                <td className="text-center">
-                  {index1 < 9 ? `0${index1 + 1}` : index1 + 1}
-                </td>
-                <td>{item.name}</td>
-                <td className="text-center">{item.value}</td>
-                <td className="text-center">{ti_le.toFixed(2)}%</td>
-              </tr>
-            );
-          })}
-          <tr>
-            <td colSpan={2} className="text-center fw-bold">
-              Tổng
-            </td>
-            <td className="text-center fw-bold">
-              {sum} ({text_name})
-            </td>
-            <td className="text-center fw-bold">100%</td>
-          </tr>
-        </tbody>
-        <tfoot />
-      </table>
-    </div>
-  );
-};
